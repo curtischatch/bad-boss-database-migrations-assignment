41ff8e53661ee4fca685bb823a1718a3393943ed
curtischatch <curtis.chat.hk@gmail.com>
2018-08-12 17:27:21 -0400

    created a migration for locations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_12_184951) do
+ActiveRecord::Schema.define(version: 2018_08_12_212230) do
 
   create_table "guests", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2018_08_12_184951) do
     t.string "illnesses"
     t.string "medication"
     t.string "voting_preferences"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "province"
+    t.string "country"
   end
 
   create_table "parts", force: :cascade do |t|
